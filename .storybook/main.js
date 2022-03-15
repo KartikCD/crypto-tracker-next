df488805d1a22f06d4de78c296c9751f55805b75
Kartik Chawda <chawdakartik@gmail.com>
2022-03-15 11:26:41 +0530

    fix: storybook styles fix.

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,15 @@
+const path = require("path");
+
 module.exports = {
 	stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
 	addons: [
+		"@storybook/addon-actions",
 		"@storybook/addon-links",
 		"@storybook/addon-essentials",
 		"@storybook/addon-interactions",
 	],
-	framework: "@storybook/react",
 	typescript: {
 		reactDocgen: false,
 	},
+	presets: [path.resolve(__dirname, "./next-preset.js")],
 };
